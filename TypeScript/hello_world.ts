f96a6bb2c479526a4846bed406b70dcdc9ab7717
Raul Troya <raultslg06@gmail.com>
2025-05-08 08:57:51 +0200

    Pruebas con TS: Añado hello_world.ts

---
+++ TypeScript/hello_world.ts
@@ -0,0 +1,16 @@
+// Sintaxis muy parecido a JS
+console.log("Hola, TypeScript!")
+
+// Variables
+// let PUEDE variar, const NO PUEDE variar
+let myString = "Esto es una cadena de texto"
+console.log(myString)
+myString = "Ha variado la cadena"
+console.log(myString)
+
+/*
+const myString = "Esto es una cadena de texto"
+console.log(myString)
+myString = "Ha variado la cadena"
+console.log(myString)
+*/
