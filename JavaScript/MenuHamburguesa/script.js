d6cba2a3a0373b6e7ed9b2247eb925642a634acf
Raul Troya <raultslg06@gmail.com>
2025-05-15 13:00:04 +0200

    Añado Login.html en la web de la VELADA

--- JavaScript/MenuHamburguesa/script.js
+++ JavaScript/MenuHamburguesa/script.js
@@ -3,9 +3,9 @@ const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
+    nav.classList.add("visible");  // Añado al nav la clase visible
 })
 
 cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
+    nav.classList.remove("visible");  // Elimino del nav la clase visible
 })
