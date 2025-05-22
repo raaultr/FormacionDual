500d0773cac3e5692839acc272363cabfce68e2d
Raul Troya <raultslg06@gmail.com>
2025-05-22 10:45:42 +0200

    Añado Apuntes basicos de JavaScript ordenados

---
+++ JavaScript/_ApuntesBasicos/01-variables.js
@@ -0,0 +1,24 @@
+// var --> Variable en desuso, es muy antigua
+
+var helloworld = "¡Hola, JavaScript!"
+console.log(helloworld)
+
+helloworld = "¡Hola de nuevo, JavaScript!"
+console.log(helloworld)
+
+// let --> Solo accesibles dentro de un bloque
+
+let helloworld2 = "¡Hola, JavaScript 2!"
+console.log(helloworld2)
+
+helloworld2 = "¡Hola de nuevo, JavaScript 2!"
+console.log(helloworld2)
+
+// const --> Una constante no puede ser reasignada como un let
+
+const helloworld3 = "¡Hola, JavaScript 3!"
+console.log(helloworld3)
+
+// Error
+// helloworld3 = "¡Hola de nuevo, JavaScript 3!"
+// console.log(helloworld3)
---
+++ JavaScript/_ApuntesBasicos/02-datatypes.js
@@ -0,0 +1,46 @@
+// Cadenas de texto (String)
+let myname = "Raul"
+let ape = "Troya"
+let email = "raultslg06@gmail.com"
+
+// Números (number)
+let age = 18  // entero
+let height = 1.75  // decimal
+
+// Booleanos (boolean)
+let isStudiants = true
+let isTeacher = false
+
+// Undefined
+let undefinedValue
+console.log(undefinedValue)
+
+// Null
+let nullValue = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+let myBigInt = BigInt(13248941247131293127354352342)
+let myBigInt2 = 13248941247131293127354352342n
+
+// Mostramos los tipos de datos
+console.log(typeof myname)
+console.log(typeof ape)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isStudiants)
+console.log(typeof isTeacher)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
